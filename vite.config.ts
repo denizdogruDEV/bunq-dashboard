c2ff6483c8c36930f720bac9b4e109b6fbb049d5
Deniz <ddogru09@gmail.com>
2025-04-06 20:42:50 +0300

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bunq-dashboard/', // This should match the repository name
   build: {
     outDir: 'build'
   }
